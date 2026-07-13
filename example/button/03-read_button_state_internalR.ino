9d8c5979fd7ea0d6ae6f9e089a1162ba9c8fa65d
Teeraphat Kullanankanjana <watit2013@gmail.com>
2026-07-13 20:28:49 +0700

    Read Button State (Internal Resistor Pull-Up)
    Read Button State (Internal Resistor Pull-Up)

---
+++ example/button/03-read_button_state_internalR.ino
@@ -0,0 +1,17 @@
+#define BTN_PIN 7
+
+int btn_state;
+
+void setup()
+{
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  btn_state = digitalRead(BTN_PIN);
+  Serial.print("ButtonState:");
+  Serial.println(btn_state);
+  delay(500);
+}
